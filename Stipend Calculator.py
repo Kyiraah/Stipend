3b977a0b676d672dfc850972b162aaab8f2d727e
Keziah Kyiraah Acquah <74784444+Kyiraah@users.noreply.github.com>
2021-02-02 00:34:44 +0000

    Create Stipend Calculator.py

---
+++ Stipend Calculator.py
@@ -0,0 +1,14 @@
+name = input ("What is your name")
+month = input("Name of month")
+hrs = input("Enter Working Hours:")
+rate = input("Enter Hourly Rate:")
+
+#Changing strings to float to allow easy calculation
+Hours = float(hrs)
+Rate = float(rate)
+
+#Calculating Pay
+Pay = print(Hours * Rate)
+
+#Declaring amount to be paid to Worker
+print("Congrats!", name, "Your", month, "income is GHS", Hours * Rate)
